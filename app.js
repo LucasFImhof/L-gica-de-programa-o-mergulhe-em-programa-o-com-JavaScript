859648fb20f066ced0f723f15bf0e26616ae0d4e
Lucas Imhof <lucasf.imhof@gmail.com>
2024-12-10 14:58:50 -0300

    Projeto com os desafios propostos na primeira etapa do curso

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(`Valor do número secreto: ${numeroSecreto}`);
-let chute;
-let tentativas = 1;
+// 1. Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+alert('Boas vindas ao nosso site!');
 
-while (chute != numeroSecreto) {
-    let chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+// 2. Declare uma variável chamada nome e atribua a ela o valor "Lua".
+// 8. Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+let nome = prompt('Informe seu nome:');
 
-    if (numeroSecreto == chute) {
-        break;
-    } else {
-        if (numeroSecreto > chute) {
-            alert(`O número secreto é maior que ${chute}`);
-        } else {
-            alert(`O número secreto é menor que ${chute}`);
-        }
-        tentativas++;
-    }
+// 3. Crie uma variável chamada idade e atribua a ela o valor 25.
+// 9. Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+let idade = prompt('Informe sua idade:');
+
+// 4. Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let numeroDeVendas = 50;
+
+// 5. Exiba um alerta com o texto "Erro! Preencha todos os campos"
+let saldoDisponivel = 1000;
+
+// 7. Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
+// 6. Exiba um alerta com o texto "Erro! Preencha todos os campos"
+if (nome == '' || idade == undefined) {
+    alert(mensagemDeErro)
+
+    // 10. Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+} else if (idade >= 18) {
+    alert('Pode tirar a habilitação!')
 }
-
-let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa'
-alert(`Isso ai! Você descobriu o número secreto é ${numeroSecreto}! Você acertou com ${tentativas} ${palavraTentativas}`); //Tamplate string permite utilizar texto junto com variáveis
